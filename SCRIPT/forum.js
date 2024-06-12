cf55350dc990f247039e0cdfca4c6924400174e1
Netanel Chertkov <netanelchertkov@gmail.com>
2024-06-12 12:37:56 +0300

    Fix firebase connection

--- SCRIPT/forum.js
+++ SCRIPT/forum.js
@@ -1,80 +1,82 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCmmjSORqrQfaw334CdAlXnYA6_rFBeGD8",
-  authDomain: "chatapp-1a0a2.firebaseapp.com",
-  projectId: "chatapp-1a0a2",
-  storageBucket: "chatapp-1a0a2.appspot.com",
-  messagingSenderId: "548724108544",
-  appId: "1:548724108544:web:ceddf339e499ef6ce4d7e5",
-  measurementId: "G-Q3L5PLRQS7"
-};
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-
-const myName = localStorage.getItem("username");
-const myPassword = localStorage.getItem("password");
-
-if (!myName || !myPassword) {
-  alert("Please log in first.");
-  window.location.href = "../index.html";
-}
-
-function sendMessage() {
-  const message = document.getElementById("message").value;
-
-  const newMessage = {
-    sender: myName,
-    password: myPassword,
-    message: message,
-    timestamp: firebase.firestore.FieldValue.serverTimestamp()
+window.onload = function() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyCmmjSORqrQfaw334CdAlXnYA6_rFBeGD8",
+    authDomain: "chatapp-1a0a2.firebaseapp.com",
+    projectId: "chatapp-1a0a2",
+    storageBucket: "chatapp-1a0a2.appspot.com",
+    messagingSenderId: "548724108544",
+    appId: "1:548724108544:web:ceddf339e499ef6ce4d7e5",
+    measurementId: "G-Q3L5PLRQS7"
   };
 
-  db.collection('messages').add(newMessage).then(() => {
-    document.getElementById("message").value = '';
-    displayMessages();
-  }).catch((error) => {
-    console.error("Error adding message: ", error);
-  });
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
 
-  return false;
-}
+  const myName = localStorage.getItem("username");
+  const myPassword = localStorage.getItem("password");
 
-function displayMessages() {
-  const messagesList = document.getElementById("messages");
-  messagesList.innerHTML = '';
+  if (!myName || !myPassword) {
+    alert("Please log in first.");
+    window.location.href = "../index.html";
+  }
 
-  db.collection('messages').orderBy('timestamp').get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-      const msg = doc.data();
-      let html = `<li id='message-${doc.id}'>`;
+  function sendMessage() {
+    const message = document.getElementById("message").value;
 
-      if (msg.sender === myName && msg.password === myPassword) {
-        html += `<button class='btn' data-id='${doc.id}' onclick='deleteMessage(this);'>Delete</button> `;
-      }
+    const newMessage = {
+      sender: myName,
+      password: myPassword,
+      message: message,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    };
 
-      html += `${msg.sender}: ${msg.message}`;
-      html += `</li><br>`;
-      messagesList.innerHTML += html;
+    db.collection('messages').add(newMessage).then(() => {
+      document.getElementById("message").value = '';
+      displayMessages();
+    }).catch((error) => {
+      console.error("Error adding message: ", error);
     });
-  }).catch((error) => {
-    console.error("Error getting messages: ", error);
-  });
-}
 
-function deleteMessage(self) {
-  const messageId = self.getAttribute("data-id");
+    return false;
+  }
 
-  db.collection('messages').doc(messageId).delete().then(() => {
-    const messageElement = document.getElementById("message-" + messageId);
-    if (messageElement) {
-      messageElement.innerHTML = "Message deleted";
-    }
-    displayMessages();
-  }).catch((error) => {
-    console.error("Error deleting message: ", error);
-  });
-}
+  function displayMessages() {
+    const messagesList = document.getElementById("messages");
+    messagesList.innerHTML = '';
 
-// Refresh messages every 3 seconds
-setInterval(displayMessages, 3000);
-displayMessages();
+    db.collection('messages').orderBy('timestamp').get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        const msg = doc.data();
+        let html = `<li id='message-${doc.id}'>`;
+
+        if (msg.sender === myName && msg.password === myPassword) {
+          html += `<button class='btn' data-id='${doc.id}' onclick='deleteMessage(this);'>Delete</button> `;
+        }
+
+        html += `${msg.sender}: ${msg.message}`;
+        html += `</li><br>`;
+        messagesList.innerHTML += html;
+      });
+    }).catch((error) => {
+      console.error("Error getting messages: ", error);
+    });
+  }
+
+  function deleteMessage(self) {
+    const messageId = self.getAttribute("data-id");
+
+    db.collection('messages').doc(messageId).delete().then(() => {
+      const messageElement = document.getElementById("message-" + messageId);
+      if (messageElement) {
+        messageElement.innerHTML = "Message deleted";
+      }
+      displayMessages();
+    }).catch((error) => {
+      console.error("Error deleting message: ", error);
+    });
+  }
+
+  // Refresh messages every 3 seconds
+  setInterval(displayMessages, 3000);
+  displayMessages();
+};
