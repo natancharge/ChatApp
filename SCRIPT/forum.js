2ad64903022daac8d9185e0b8d0a30b657ed0cac
Netanel Chertkov <netanelchertkov@gmail.com>
2024-06-12 12:27:15 +0300

    Set up firebase

--- SCRIPT/forum.js
+++ SCRIPT/forum.js
@@ -1,17 +1,15 @@
-window.onload = function() {
-  const firebaseConfig = {
-    apiKey: "AIzaSyCmmjSORqrQfaw334CdAlXnYA6_rFBeGD8",
-    authDomain: "chatapp-1a0a2.firebaseapp.com",
-    projectId: "chatapp-1a0a2",
-    storageBucket: "chatapp-1a0a2.appspot.com",
-    messagingSenderId: "548724108544",
-    appId: "1:548724108544:web:ceddf339e499ef6ce4d7e5",
-    measurementId: "G-Q3L5PLRQS7"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyCmmjSORqrQfaw334CdAlXnYA6_rFBeGD8",
+  authDomain: "chatapp-1a0a2.firebaseapp.com",
+  projectId: "chatapp-1a0a2",
+  storageBucket: "chatapp-1a0a2.appspot.com",
+  messagingSenderId: "548724108544",
+  appId: "1:548724108544:web:ceddf339e499ef6ce4d7e5",
+  measurementId: "G-Q3L5PLRQS7"
+};
 
-  firebase.initializeApp(firebaseConfig);
-  const db = firebase.firestore();
-}
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
 const myName = localStorage.getItem("username");
 const myPassword = localStorage.getItem("password");
@@ -23,55 +21,60 @@ if (!myName || !myPassword) {
 
 function sendMessage() {
   const message = document.getElementById("message").value;
-  const database = localStorage.getItem('database');
-  const messages = database ? JSON.parse(database) : [];
 
   const newMessage = {
-    key: Date.now().toString(),
     sender: myName,
     password: myPassword,
     message: message,
-    timestamp: new Date().toISOString()
+    timestamp: firebase.firestore.FieldValue.serverTimestamp()
   };
 
-  messages.push(newMessage);
-  localStorage.setItem('database', JSON.stringify(messages));
-  document.getElementById("message").value = '';
+  db.collection('messages').add(newMessage).then(() => {
+    document.getElementById("message").value = '';
+    displayMessages();
+  }).catch((error) => {
+    console.error("Error adding message: ", error);
+  });
+
   return false;
 }
 
 function displayMessages() {
   const messagesList = document.getElementById("messages");
-  const database = localStorage.getItem('database');
-  const messages = database ? JSON.parse(database) : [];
-
   messagesList.innerHTML = '';
 
-  messages.forEach(function(msg) {
-    let html = `<li id='message-${msg.key}'>`;
+  db.collection('messages').orderBy('timestamp').get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      const msg = doc.data();
+      let html = `<li id='message-${doc.id}'>`;
 
-    if (msg.sender === myName && msg.password === myPassword) {
-      html += `<button class='btn' data-id='${msg.key}' onclick='deleteMessage(this);'>Delete</button> `;
-    }
+      if (msg.sender === myName && msg.password === myPassword) {
+        html += `<button class='btn' data-id='${doc.id}' onclick='deleteMessage(this);'>Delete</button> `;
+      }
 
-    html += `${msg.sender}: ${msg.message}`;
-    html += `</li><br>`;
-    messagesList.innerHTML += html;
+      html += `${msg.sender}: ${msg.message}`;
+      html += `</li><br>`;
+      messagesList.innerHTML += html;
+    });
+  }).catch((error) => {
+    console.error("Error getting messages: ", error);
   });
 }
 
 function deleteMessage(self) {
   const messageId = self.getAttribute("data-id");
-  const database = localStorage.getItem('database');
-  const messages = database ? JSON.parse(database) : [];
-  const updatedMessages = messages.filter(msg => msg.key !== messageId);
 
-  localStorage.setItem('database', JSON.stringify(updatedMessages));
-  const messageElement = document.getElementById("message-" + messageId);
-  if (messageElement) {
-    messageElement.innerHTML = "Message deleted";
-  }
+  db.collection('messages').doc(messageId).delete().then(() => {
+    const messageElement = document.getElementById("message-" + messageId);
+    if (messageElement) {
+      messageElement.innerHTML = "Message deleted";
+    }
+    displayMessages();
+  }).catch((error) => {
+    console.error("Error deleting message: ", error);
+  });
 }
 
+// Refresh messages every 3 seconds
 setInterval(displayMessages, 3000);
 displayMessages();
